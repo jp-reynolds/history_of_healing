d16f77fd297205111ff2faba22be176c21df4424
jp-reynolds <jpreynolds215@gmail.com>
2018-08-19 20:27:23 -0600

    dummy text

--- src/components/episodes/Episodes.js
+++ src/components/episodes/Episodes.js
@@ -10,23 +10,23 @@ class Episodes extends Component {
         <Header/>
         <section className="episodes">
         	<div className="episode">
-        		<div className="imageEpisode"></div>
-        		<div className="descriptionEpisode"></div>
+        		<img alt="Doctor" className="imageEpisode" src="https://blog.wellcomelibrary.org/wp-content/uploads/2013/09/1d4fcd7968c4df503994dd8b8af.jpg"/>
+        		<p className="descriptionEpisode">One two three four five six seven eigth nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty</p>
         		<div className="controlsEpisode"></div>
             </div>
         	<div className="episode">
-        		<div className="imageEpisode"></div>
-        		<div className="descriptionEpisode"></div>
+        		<img alt="Doctor" className="imageEpisode" src="https://blog.wellcomelibrary.org/wp-content/uploads/2013/09/1d4fcd7968c4df503994dd8b8af.jpg"/>
+        		<p className="descriptionEpisode">One two three four five six seven eigth nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty</p>
         		<div className="controlsEpisode"></div>
             </div>
         	<div className="episode">
-        		<div className="imageEpisode"></div>
-        		<div className="descriptionEpisode"></div>
+        		<img alt="Doctor" className="imageEpisode" src="https://blog.wellcomelibrary.org/wp-content/uploads/2013/09/1d4fcd7968c4df503994dd8b8af.jpg"/>
+        		<p className="descriptionEpisode">One two three four five six seven eigth nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty</p>
         		<div className="controlsEpisode"></div>
             </div>
         	<div className="episode">
-        		<div className="imageEpisode"></div>
-        		<div className="descriptionEpisode"></div>
+        		<img alt="Doctor" className="imageEpisode" src="https://blog.wellcomelibrary.org/wp-content/uploads/2013/09/1d4fcd7968c4df503994dd8b8af.jpg"/>
+        		<p className="descriptionEpisode">One two three four five six seven eigth nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty</p>
         		<div className="controlsEpisode"></div>
             </div>
         </section>
